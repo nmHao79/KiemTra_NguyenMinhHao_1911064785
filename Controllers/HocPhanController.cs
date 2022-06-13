410e302c5927d3f68e3108202ca6323a01ad632c
MinhHaoC1 <nmhao.git@gmail.com>
2022-06-13 16:06:34 +0700

    KiemTra_NguyenMinhHao

---
+++ Controllers/HocPhanController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KiemTra_NguyenMinhHao.Models;
+
+namespace KiemTra_NguyenMinhHao.Controllers
+{
+    public class HocPhanController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        // GET: HocPhan
+        public ActionResult Index()
+        {
+            var E_Hp = from hp in data.HocPhans select hp;
+            return View(E_Hp);
+        }
+    }
+}
---
+++ Controllers/SinhVienController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KiemTra_NguyenMinhHao.Models;
+namespace KiemTra_NguyenMinhHao.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Detail(string id)
+        {
+            var E_Sv = data.SinhViens.Where(m => m.MaSV == id).First();
+            return View(E_Sv);
+        }
+        public ActionResult Edit(string id)
+        {
+            var SinhVienS = new List<SinhVien>();
+            var E_Sv = data.SinhViens.First(m => m.MaSV == id);
+            return View(E_Sv);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, SinhVien sv)
+        {
+            
+            var ten = collection["HoTen"];
+            if (string.IsNullOrEmpty(ten))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                
+                sv.HoTen = ten;
+                data.SinhViens.InsertOnSubmit(sv);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Create();
+        }
+        public ActionResult Delete(string id)
+        {
+            var D_Sv = data.SinhViens.First(m => m.MaSV == id);
+            return View(D_Sv);
+        }
+        [HttpPost]
+        public ActionResult Delete(string id, FormCollection collection)
+        {
+            var sv = data.SinhViens.Where(m => m.MaSV == id).First();
+            data.SinhViens.DeleteOnSubmit(sv);
+            data.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+        // GET: SinhVien
+        public ActionResult Index()
+        {
+            var E_Sv = from sv in data.SinhViens select sv;
+            return View(E_Sv);
+        }
+
+    }
+}
